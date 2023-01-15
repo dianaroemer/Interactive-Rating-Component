f3ee7dfeb65a141be1846491918e4bb046530da2
D Roemer <dominicroemer@gmail.com>
2023-01-14 20:55:14 -0800

    Initial commit of create-react-app build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './Components/InteractiveRatingComponent';
+import InteractiveRatingComponent from './Components/InteractiveRatingComponent';
 
 function App() {
   return (
@@ -18,7 +20,12 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <InteractiveRatingComponent/>    
     </div>
+
+    
+
   );
 }
 
---
+++ src/Components/InteractiveRatingComponent.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import '../Styles/InteractiveRatingComponentStyle.scss';
+
+function InteractiveRatingComponent(props) {
+
+    return(
+        <div className='doop'>
+            Hello World, I am InteractiveRatingComponent.js
+        </div>
+    );
+
+}
+
+export default InteractiveRatingComponent;
