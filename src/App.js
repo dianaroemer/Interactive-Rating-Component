e40af88b266ad292636be8ed77c7e57ab193cc4d
D Roemer <dominicroemer@gmail.com>
2023-01-14 21:51:51 -0800

    move footer to App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ function App() {
 
       <InteractiveRatingComponent/>    
 
+      <footer class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+      Coded by <a href="https://github.com/dianaroemer">Diana Roemer</a>.
+      </footer>
+
     </div>
 
     
